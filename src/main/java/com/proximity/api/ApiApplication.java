1453b158a95e8b54b167f14c37ab7c7adbb64726
chandlergreff <I433816@fhict.nl>
2022-01-05 06:13:10 +0100

    added tomtom and setup

--- src/main/java/com/proximity/api/ApiApplication.java
+++ src/main/java/com/proximity/api/ApiApplication.java
@@ -1,13 +1,24 @@
 package com.proximity.api;
 
+import com.proximity.api.tomtom.Tomtom;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+	
+	@GetMapping
+	public List<String> getQueryResult() {
+		return Tomtom.queryApi("beach");
 	}
 
 }
